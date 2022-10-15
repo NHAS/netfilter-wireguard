1e713ec4408278ce41bf8c68decb26c62e40e35f
NHAS <jordanatararimu@gmail.com>
2022-10-15 20:30:33 +1300

    Add/delete/set ip examples

--- go.mod
+++ go.mod
@@ -7,10 +7,13 @@ require (
 	github.com/josharian/native v1.0.0 // indirect
 	github.com/mdlayher/genetlink v1.2.0 // indirect
 	github.com/mdlayher/netlink v1.6.0 // indirect
-	github.com/mdlayher/socket v0.1.1 // indirect
+	github.com/mdlayher/socket v0.2.3 // indirect
 	github.com/vishvananda/netlink v1.1.0 // indirect
 	github.com/vishvananda/netns v0.0.0-20191106174202-0a2b9b5464df // indirect
-	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
+	golang.org/x/net v0.0.0-20220418201149-a630d4f3e7a2 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
-	golang.org/x/sys v0.0.0-20220128215802-99c3d69c2c27 // indirect
+	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
+	golang.zx2c4.com/wireguard v0.0.0-20220407013110-ef5c587f782d // indirect
+	golang.zx2c4.com/wireguard/wgctrl v0.0.0-20220916014741-473347a5e6e3 // indirect
 )
--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"log"
+	"net"
+	"time"
 	"unsafe"
 
 	"github.com/mdlayher/netlink"
 	"golang.org/x/sys/unix"
+	"golang.zx2c4.com/wireguard/wgctrl"
+	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
 )
 
 type IfInfomsg struct {
@@ -23,13 +28,19 @@ func (msg *IfInfomsg) Serialize() []byte {
 	return (*(*[unix.SizeofIfInfomsg]byte)(unsafe.Pointer(msg)))[:]
 }
 
-func main() {
+type IfAddrmsg struct {
+	Family    uint8
+	Prefixlen uint8
+	Flags     uint8
+	Scope     uint8
+	Index     uint32
+}
 
-	conn, err := netlink.Dial(unix.NETLINK_ROUTE, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
+func (msg *IfAddrmsg) Serialize() []byte {
+	return (*(*[unix.SizeofIfAddrmsg]byte)(unsafe.Pointer(msg)))[:]
+}
+
+func addWg(c *netlink.Conn, name string, address net.IPNet, mtu int) error {
 
 	infomsg := IfInfomsg{
 		Family: unix.AF_UNSPEC,
@@ -38,8 +49,8 @@ func main() {
 	}
 
 	ne := netlink.NewAttributeEncoder()
-	ne.Int32(unix.IFLA_MTU, 1420)
-	ne.String(unix.IFLA_IFNAME, "wg3")
+	ne.Int32(unix.IFLA_MTU, int32(mtu))
+	ne.String(unix.IFLA_IFNAME, name)
 
 	ne.Nested(unix.IFLA_LINKINFO, func(nae *netlink.AttributeEncoder) error {
 		nae.String(unix.IFLA_INFO_KIND, unix.WG_GENL_NAME)
@@ -57,14 +68,14 @@ func main() {
 
 	msg, err := ne.Encode()
 	if err != nil {
-		log.Fatal("failed to encode: ", err)
+		return fmt.Errorf("failed to encode: %v", err)
 	}
 
 	req.Data = append(req.Data, msg...)
 
-	resp, err := conn.Execute(req)
+	resp, err := c.Execute(req)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("failed to execute message: %v", err)
 	}
 
 	switch resp[0].Header.Type {
@@ -72,12 +83,167 @@ func main() {
 		errCode := binary.LittleEndian.Uint32(resp[0].Data)
 		if errCode != 0 {
 			fmt.Println("Netlink reported error: ", errCode)
-			return
+			return errors.New("got netlink error: " + fmt.Sprintf("%d", errCode))
 		}
 	}
 
-	fmt.Println("All goods!")
+	return setIp(c, name, address)
+}
 
-	conn.Receive()
+func setIp(c *netlink.Conn, name string, address net.IPNet) error {
 
+	req := netlink.Message{
+		Header: netlink.Header{
+			Type:  unix.RTM_NEWADDR,
+			Flags: netlink.Request | netlink.Acknowledge,
+		},
+	}
+
+	iface, err := net.InterfaceByName(name)
+	if err != nil {
+		return fmt.Errorf("lookup network iface %q: %s", name, err)
+	}
+
+	addrMsg := IfAddrmsg{
+		Family: unix.AF_INET,
+		Index:  uint32(iface.Index),
+	}
+
+	preflen, _ := address.Mask.Size()
+	addrMsg.Prefixlen = uint8(preflen)
+
+	req.Data = addrMsg.Serialize()
+
+	ne := netlink.NewAttributeEncoder()
+	ne.Bytes(unix.IFA_LOCAL, address.IP)
+
+	msg, err := ne.Encode()
+	if err != nil {
+		return fmt.Errorf("failed to encode af: %v", err)
+	}
+
+	req.Data = append(req.Data, msg...)
+
+	resp, err := c.Execute(req)
+	if err != nil {
+		return fmt.Errorf("failed to execute message: %v", err)
+	}
+
+	switch resp[0].Header.Type {
+	case netlink.Error:
+		errCode := binary.LittleEndian.Uint32(resp[0].Data)
+		if errCode != 0 {
+			fmt.Println("Netlink reported error: ", errCode)
+			return errors.New("got netlink error: " + fmt.Sprintf("%d", errCode))
+		}
+	}
+
+	return nil
+}
+
+func delWg(c *netlink.Conn, name string) error {
+	infomsg := IfInfomsg{
+		Family: unix.AF_UNSPEC,
+		Change: unix.IFF_UP | unix.IFF_LOWER_UP,
+		Flags:  unix.IFF_UP | unix.IFF_LOWER_UP,
+	}
+
+	ne := netlink.NewAttributeEncoder()
+	ne.String(unix.IFLA_IFNAME, name)
+
+	ne.Nested(unix.IFLA_LINKINFO, func(nae *netlink.AttributeEncoder) error {
+		nae.String(unix.IFLA_INFO_KIND, unix.WG_GENL_NAME)
+		return nil
+	})
+
+	req := netlink.Message{
+		Header: netlink.Header{
+			Type:  unix.RTM_DELLINK,
+			Flags: netlink.Request | netlink.Acknowledge,
+		},
+	}
+
+	req.Data = infomsg.Serialize()
+
+	msg, err := ne.Encode()
+	if err != nil {
+		return fmt.Errorf("failed to encode: %v", err)
+	}
+
+	req.Data = append(req.Data, msg...)
+
+	resp, err := c.Execute(req)
+	if err != nil {
+		return fmt.Errorf("failed to execute message: %v", err)
+	}
+
+	switch resp[0].Header.Type {
+	case netlink.Error:
+		errCode := binary.LittleEndian.Uint32(resp[0].Data)
+		if errCode != 0 {
+			fmt.Println("Netlink reported error: ", errCode)
+			return errors.New("got netlink error: " + fmt.Sprintf("%d", errCode))
+		}
+	}
+
+	return nil
+}
+
+func main() {
+
+	conn, err := netlink.Dial(unix.NETLINK_ROUTE, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	const devName = "wg2"
+	fmt.Print("Creating wireguard device...")
+
+	_, n, _ := net.ParseCIDR("192.168.2.1/32")
+	if err := addWg(conn, devName, *n, 1420); err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Done!")
+
+	fmt.Print("Connecting to wireguard control...")
+	ctrl, err := wgctrl.New()
+	if err != nil {
+		fmt.Printf("cannot start wireguard control %v\n", err)
+		return
+	}
+	fmt.Println("Done!")
+
+	fmt.Print("Using wireguard control to set configuration...")
+	pk, err := wgtypes.GeneratePrivateKey()
+	if err != nil {
+		fmt.Printf("cannot generate key %v\n", err)
+		return
+	}
+
+	port := 3333
+
+	c := wgtypes.Config{
+		PrivateKey: &pk,
+		ListenPort: &port,
+	}
+
+	err = ctrl.ConfigureDevice(devName, c)
+	if err != nil {
+		fmt.Printf("cannot configure wireguard device %v\n", err)
+		return
+	}
+	fmt.Println("Done!")
+
+	fmt.Print("Waiting 15 seconds...")
+	time.Sleep(15 * time.Second)
+	fmt.Println("Done!")
+
+	fmt.Print("Deleting wireguard device...")
+	if err := delWg(conn, devName); err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Done!")
 }
